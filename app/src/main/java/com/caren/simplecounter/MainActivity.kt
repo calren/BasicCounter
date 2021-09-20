72e3236b0f9aba2114dadd09c8a20ef90f3390dc
Caren Chang <carenc@google.com>
2021-09-20 09:24:13 -0700

    First commit

---
+++ app/src/main/java/com/caren/simplecounter/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.caren.simplecounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    var number = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val numberTextView = findViewById<TextView>(R.id.number)
+
+        findViewById<Button>(R.id.subtract).setOnClickListener {
+            number--
+            numberTextView.text = number.toString()
+        }
+
+        findViewById<Button>(R.id.add).setOnClickListener {
+            number++
+            numberTextView.text = number.toString()
+        }
+    }
+}
